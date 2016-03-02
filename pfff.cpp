b85c86b94325067979e5dbbbf399a708f2d980ea
Администратор <ytr-95@mail.ru>
2016-03-02 11:02:55 +0400

    some fix K

--- pfff.cpp
+++ pfff.cpp
@@ -7,31 +7,32 @@
 	#include <iomanip>
 	#include <algorithm>
 	#include <math.h>
+
 #include <list>
 	#include <fstream>
 	# define P          3.141592653589793238462643383279502884L	 /* pi */
 	# define MOD 1000000007;
 	using namespace std;
 
-	double k = 6.0;
+	double k = 1.0;
 	double eps = 0.0000001;
-	int needPod=6;
+	int needPod=3;
 	int n=500;
 	ofstream ans;
 	double f(double x1, double x2){
-			return 180.45*x1*x1-k*29.7*x1*x2+(0.05*k*k+45)*x2*x2-1/k*(k*k+9)*x1-1/k*(k*k+9)*x2;}
+			return (5.0*k*k+0.45)*x1*x1-k*29.7*x1*x2+(0.05*k*k+45)*x2*x2-1/k*(k*k+9)*x1-1/k*(k*k+9)*x2;}
 
 	double df1 (double x1, double x2,int i)
 	{
-		if (i==1) return (7.5+178.2*x2)/360.9;
-		if (i==2) return (360.9*x1-7.5)/178.2;
+		if (i==1) return ((k*k+9.0)/k+29.7*k*x2)/(2*5*k*k+0.9);
+		if (i==2) return ((2*k*k+0.9)*x1-(k*k+9.0)/k)/(29.7*k);
 
 	}
 	double df2 (double x1, double x2,int i)
 	{
 		
-		if (i==1) return (93.6*x2-7.5)/178.2;
-	    if (i==2) return (7.5+178.2*x1)/93.6;
+		if (i==1) return ((2*k*k*0.05+90)*x2-(k*k+9.0)/k)/(29.7*k);
+	    if (i==2) return ((k*k+9.0)/k+29.7*k*x1)/(2*k*k*0.05+90);
 	}
 
 	void pokoord(double x1, double x2, int n1, int n2){
@@ -85,7 +86,7 @@
 
 			if (pred>=fe) {
 				   jz1(pod+1,a,b,c,d*3,e,t,fe); 
-			}else  jz1(0,x1,x2,h1,d*(-0.5),m1,m2,f(x1,x2));
+			}else  jz1(0,x1,x2,h1,d*(-0.5),e,t,f(x1,x2));
 
 		} else 
 		 { 
@@ -107,7 +108,7 @@
 			
 
 			if (pred>=fe) jz1(pod+1,a,b,c*3,d,e,t,fe); 
-			else     	  jz1(0,x1,x2,c*(-0.5),d,m1,m2,f(x1,x2));
+			else     	  jz1(0,x1,x2,c*(-0.5),d,e,t,f(x1,x2));
 		}
 
 	
@@ -117,14 +118,17 @@
 	
 
 	int main(){
+		
 
 		ans.open("out.txt");
 	
 		double x1=(double)-1,x2=(double)-1;
 		
-		//jz1(0,-1.0,-1.0,1.0/6.0,1.0/6.0,0,0,f(x1,x2));
+		jz1(0,-1.0,-1.0,1.0/k,1.0/k,0,0,f(x1,x2));
 		//pokoord(-1,-1,0,0);
 		ans.close();
+		
 		system("pause");	
+		
 		return 0;
 	}
